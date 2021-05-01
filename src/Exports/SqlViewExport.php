527800d3b2f4a5476fcf04f122f8683c75e6ac98
dave-mills <dave-mills@users.noreply.github.com>
2021-05-01 22:09:55 +0000

    Fix styling

--- src/Exports/SqlViewExport.php
+++ src/Exports/SqlViewExport.php
@@ -4,14 +4,12 @@
 namespace Stats4sd\KoboLink\Exports;
 
 use Illuminate\Support\Facades\DB;
-use App\Models\Team;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class SqlViewExport implements \Maatwebsite\Excel\Concerns\FromCollection, WithHeadings
 {
     public string $viewName;
     public $team;
-
 
     public function __construct(string $viewName, $team = null)
     {
@@ -23,11 +21,14 @@ class SqlViewExport implements \Maatwebsite\Excel\Concerns\FromCollection, WithH
     {
         if ($this->team) {
             $collection = DB::table($this->viewName)->where('team_id', '=', $this->team->id)->get();
+
             return $collection->map(function ($item) {
                 unset($item->team_id);
+
                 return $item;
             });
         }
+
         return DB::table($this->viewName)->get();
     }
 
