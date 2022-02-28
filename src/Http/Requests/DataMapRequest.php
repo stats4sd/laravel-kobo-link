e327fa09b970f16f143e0b3a9c8bcee69de2d5a5
Dan Tang <dan@stats4sd.org>
2022-02-28 17:13:34 +0000

    Add request classes

---
+++ src/Http/Requests/DataMapRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Stats4sd\KoboLink\Http\Requests;
+
+use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
+
+class DataMapRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|max:255|alpha_dash',
+            'description' => 'nullable',
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'id|alpha_dash' => 'The ID cannot have any spaces in the entry',
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
